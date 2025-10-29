a7b9c615f2de56ea28248b2bc4d8931bb6169e84
Asus <agavindu123@gmail.com>
2025-10-29 18:50:21 +0530

    Add GitHub API integration for user and repository retrieval

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
-import axios from "axios"
-import dotenv from "dotenv"
-dotenv.config()
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+const github = axios.create({
+  baseURL: "https://api.github.com",
+  headers: {
+    Authorization: `token ${process.env.GITHUB_API_KEY}`,
+    Accept: "application/vnd.github+json",
+  },
+});
+
+const getUser = async (userName) => {
+  try {
+    const response = await github.get(`/users/${userName}`);
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error", error.response?.data || error.message);
+  }
+};
+
+getUser("Gavinduachintha");
+
+const getRepos = async (userName) => {
+  try {
+    const response = await axios.get(`/users/${userName}/repos`);
+    response.data.foreach((repo) => console.log(repo.name));
+  } catch (error) {
+    console.error("Error: ",error.response?.data||error.message);
+    
+  }
+};
+
+getRepos("Gavinduachintha")

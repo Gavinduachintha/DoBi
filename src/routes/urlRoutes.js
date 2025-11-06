e8485cdf2b9baf5c4d633bf5d27722803eb676d4
Asus <agavindu123@gmail.com>
2025-11-06 07:39:15 +0530

    Add login endpoint to URL routes and update urlController

--- src/routes/urlRoutes.js
+++ src/routes/urlRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { healthCheck} from "../controllers/urlController.js";
+import { healthCheck,login} from "../controllers/urlController.js";
 
 const Router = express.Router();
 Router.get("/", healthCheck);
-
+Router.get("/login",login)
 
 export default Router;
